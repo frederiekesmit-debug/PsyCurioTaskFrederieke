9c73bac64e220c8d7086cbac3809944e66dfcf54
Fre <frederieke.smit@ue-germany.de>
2026-06-08 18:50:31 +0200

    made the prefabs spawn smaller on the counter

--- TaskPsycurio_Frederieke/Assets/Scripts/Counter.cs
+++ TaskPsycurio_Frederieke/Assets/Scripts/Counter.cs
@@ -4,6 +4,8 @@ public class Counter : MonoBehaviour
 {
     [SerializeField] private Transform[] slots = new Transform[5];
 
+    [SerializeField] private Vector3 placedScale = new Vector3(0.5f, 0.5f, 0.5f);
+
     private int nextSlotIndex = 0;
 
     public bool TryPlace(GameObject obj)
@@ -16,8 +18,12 @@ public class Counter : MonoBehaviour
 
         Transform slot = slots[nextSlotIndex];
 
+        // Move to slot
         obj.transform.position = slot.position;
         obj.transform.rotation = slot.rotation;
+
+        // Make smaller on counter
+        obj.transform.localScale = placedScale;
 
         nextSlotIndex++;
 
